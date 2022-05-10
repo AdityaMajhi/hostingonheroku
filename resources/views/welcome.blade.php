ba7d35a2b262f354ce39b10954d8494385a3999b
aashikmajhi <aashikmajhi@gmail.com>
2022-05-10 16:18:43 +0545

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 @section('page')
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	<div class="container">
-		<a class="navbar-brand" href="">Safari<span>Travel Agency</span></a>
+		<a class="navbar-brand" href="">Travel<span>Travel Agency</span></a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
 			aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="oi oi-menu"></span> Menu
